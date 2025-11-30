c47d6279b26d3391d568cf8c25232f08a8965362
PANDIT VIVEK KUMAR <vivek.pandit3190@gmail.com>
2025-11-30 19:25:02 +0530

    java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = addNumbers(3, 7);
+        System.out.println(result);
+    }
+}
